bacabf2799e8f531aca0366830cf4b710e527e7f
rupesh <rupes@Bansal>
2025-01-22 21:10:54 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ import express from "express";
     const connectDB  = async()=>{
         try{
        await mongoose.connect(process.env.MONOGO_URL);
-        console.log("Connected to MongoDB")
+        console.log("Connected to mongo")
     } 
     catch(error){
         console.log(`Connecting to the Server ${error}`)
